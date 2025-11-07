e119287c58402ab6af880adb9b20fec62ed1fdeb
QAProjectManagement <mahfudcovra@gmail.com>
2025-11-07 10:36:43 +0700

    update code

--- dashboard_qa.py
+++ dashboard_qa.py
@@ -484,7 +484,7 @@ def main() -> None:
         format_func=lambda item: item[1],
     )[0]
 
-    file_path = r"data/evidence/data_procesor/qualityReportIDBTest_September.xlsx"
+    file_path = r"data/evidence/data_procesor/qualityReportIDBTest_October.xlsx"
     weekly_file_path = r"data/evidence/data_procesor/Weekly/All Project - Weekly Report.xlsx"
 
     xls: Optional[pd.ExcelFile] = None
